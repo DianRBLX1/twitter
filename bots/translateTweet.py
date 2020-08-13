f7bcf2605c8c0a2d1329ae3d3968a24f6d7be224
Nishank Sisodiya <nishanksisodiya@gmail.com>
2020-08-13 13:38:31 +0530

    generalized bot

--- bots/translateTweet.py
+++ bots/translateTweet.py
@@ -35,9 +35,26 @@ def check_new_tweet(api, since_id):
             except Exception as e:
                 logger.error("Error on fav", exc_info=True)
 
-            api.update_status(
-                status=f"{result}\nhttps://twitter.com/{tweet.user.screen_name}/status/{tweet.id}",
-            )
+            orig_twe = f"{result}\nhttps://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
+            if len(orig_twe) > 280:
+                split_from = len(orig_twe)-280
+                for i in range(split_from, 0, -1):
+                    if orig_twe[i] == " ":
+                        split_from = i
+                        break
+
+                twe = api.update_status(
+                    status=f"{result[:split_from]}\nhttps://twitter.com/{tweet.user.screen_name}/status/{tweet.id}",
+                )
+                api.update_status(
+                    status=f"{result[split_from:]}",
+                    in_reply_to_status_id=twe.id
+                )
+            else:
+                api.update_status(
+                    status=orig_twe,
+                    in_reply_to_status_id=twe.id
+                )
 
     logger.info('fetched')
     return new_since_id
